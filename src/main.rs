4eefd990e58b8b6aa35c31b9b35481eb870ff391
_Bare <bare@logixsr.com>
2021-10-11 12:56:38 -0400

    pedantic refactors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    env, fs,
+    env,
+    ffi::OsStr,
+    fs,
     io::{self, stdout, StdoutLock, Write},
     path::PathBuf,
 };
@@ -10,17 +12,17 @@ struct Counts {
 }
 
 fn walk(out: &mut StdoutLock, dir: &str, prefix: &str, counts: &mut Counts) -> io::Result<()> {
-    let mut paths = fs::read_dir(dir)?.filter_map(|e| e.ok()).map(|e| e.path()).collect::<Vec<PathBuf>>();
+    let mut paths = fs::read_dir(dir)?.filter_map(Result::ok).map(|e| e.path()).collect::<Vec<PathBuf>>();
     let mut index = paths.len();
 
     paths.sort_by(|a, b| {
-        let aname = a.file_name();
-        let bname = b.file_name();
-        aname.cmp(&bname)
+        let a_name = a.file_name();
+        let b_name = b.file_name();
+        a_name.cmp(&b_name)
     });
 
     for path in paths {
-        let name = if let Some(name) = path.file_name().and_then(|f| f.to_str()) {
+        let name = if let Some(name) = path.file_name().and_then(OsStr::to_str) {
             name
         } else {
             continue;
@@ -28,7 +30,7 @@ fn walk(out: &mut StdoutLock, dir: &str, prefix: &str, counts: &mut Counts) -> i
 
         index -= 1;
 
-        if name.starts_with(".") {
+        if name.starts_with('.') {
             continue;
         }
 
@@ -39,14 +41,14 @@ fn walk(out: &mut StdoutLock, dir: &str, prefix: &str, counts: &mut Counts) -> i
         }
 
         if index == 0 {
-            let _ = writeln!(out, "{}└── {}", prefix, name);
+            let _w = writeln!(out, "{}\u{2514}\u{2500}\u{2500} {}", prefix, name);
             if path.is_dir() {
                 walk(out, &format!("{}/{}", dir, name), &format!("{}    ", prefix), counts)?;
             }
         } else {
-            let _ = writeln!(out, "{}├── {}", prefix, name);
+            let _w = writeln!(out, "{}\u{251c}\u{2500}\u{2500} {}", prefix, name);
             if path.is_dir() {
-                walk(out, &format!("{}/{}", dir, name), &format!("{}│   ", prefix), counts)?;
+                walk(out, &format!("{}/{}", dir, name), &format!("{}\u{2502}   ", prefix), counts)?;
             }
         }
     }
@@ -58,15 +60,15 @@ fn main() -> io::Result<()> {
     let out = stdout();
     let mut out = out.lock();
 
-    let dir = env::args().nth(1).unwrap_or(".".to_string());
-    let _ = writeln!(out, "{}", dir);
+    let dir = env::args().nth(1).unwrap_or_else(|| '.'.to_string());
+    let _w = writeln!(out, "{}", dir);
 
     let mut counts = Counts { dirs: 0, files: 0 };
     walk(&mut out, &dir, "", &mut counts)?;
 
-    let _ = writeln!(out, "\n{} directories, {} files", counts.dirs, counts.files);
+    let _w = writeln!(out, "\n{} directories, {} files", counts.dirs, counts.files);
 
-    let _ = out.flush();
+    let _w = out.flush();
 
     Ok(())
 }
