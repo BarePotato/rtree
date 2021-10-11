491dfb2d6ac48ae31cc4f9bbcfc5b1a57deac643
_Bare <bare@logixsr.com>
2021-10-11 16:07:52 -0400

    clippy; rename var

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ struct Counts {
 fn walk(out: &mut BufWriter<StdoutLock>, dir: &str, prefix: &str, counts: &mut Counts) -> io::Result<()> {
     let mut paths = fs::read_dir(dir)?
         .filter_map(Result::ok)
-        .filter(|p| !p.path().is_symlink())
-        .filter_map(|e| e.path().file_name().and_then(OsStr::to_str).map(|s| (s.to_string(), e.path().is_dir())))
+        .filter(|de| !de.path().is_symlink())
+        .filter_map(|de| de.path().file_name().and_then(OsStr::to_str).map(|s| (s.to_string(), de.path().is_dir())))
         .collect::<Vec<(String, bool)>>();
     let mut index = paths.len();
 
@@ -37,12 +37,12 @@ fn walk(out: &mut BufWriter<StdoutLock>, dir: &str, prefix: &str, counts: &mut C
         }
 
         if index == 0 {
-            let _w = write!(out, "{}\u{2514}\u{2500}\u{2500} {}\n", prefix, file_name);
+            let _w = writeln!(out, "{}\u{2514}\u{2500}\u{2500} {}", prefix, file_name);
             if is_dir {
                 walk(out, &format!("{}/{}", dir, file_name), &format!("{}    ", prefix), counts)?;
             }
         } else {
-            let _w = write!(out, "{}\u{251c}\u{2500}\u{2500} {}\n", prefix, file_name);
+            let _w = writeln!(out, "{}\u{251c}\u{2500}\u{2500} {}", prefix, file_name);
             if is_dir {
                 walk(out, &format!("{}/{}", dir, file_name), &format!("{}\u{2502}   ", prefix), counts)?;
             }
@@ -59,12 +59,12 @@ fn main() -> io::Result<()> {
     let mut out = BufWriter::new(out);
 
     let dir = env::args().nth(1).unwrap_or_else(|| '.'.to_string());
-    let _w = write!(out, "{}\n", dir);
+    let _w = writeln!(out, "{}", dir);
 
     let mut counts = Counts { dirs: 0, files: 0 };
     walk(&mut out, &dir, "", &mut counts)?;
 
-    let _w = write!(out, "\n{} directories, {} files\n", counts.dirs, counts.files);
+    let _w = writeln!(out, "\n{} directories, {} files", counts.dirs, counts.files);
 
     Ok(())
 }
